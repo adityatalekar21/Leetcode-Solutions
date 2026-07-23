3107c0728c19cabde7e436f06d634ad0e333578a
adityatalekar21 <148623545+adityatalekar21@users.noreply.github.com>
2026-07-24 00:21:02 +0530

    Time: 1 ms (50.35%), Space: 46.9 MB (49.5%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            if (nums[left] % 2 == 0) {
-                left++;
-            } else if (nums[right] % 2 != 0) {
-                right--;
-            } else {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
+        int ans[] = new int[nums.length];
+        int i = 0;
+        for(int num : nums){
+            if(num % 2 == 0){
+                ans[i] = num;
+                i++;
             }
-
         }
-        return nums;
-
+        for(int num : nums){
+            if(num % 2 != 0){
+                ans[i] = num;
+                i++;
+            }
+        }
+        return ans;
     }
 }
